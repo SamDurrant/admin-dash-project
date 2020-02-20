e763204df4731c4150a0bb53421b68b65245035c
Samantha Durrant <samantha.durrant.87@gmail.com>
2020-02-19 18:16:47 -0700

    add dataTables to use pagination

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+import data from './data.js';
+
 function main() {
   $('[data-toggle="tooltip"]').tooltip();
+  $('#itemTable').DataTable({
+    data: data.dataSet,
+    columns: [
+      { title: "Amount" },
+      { title: "Name" },
+      { title: "Calories" },
+      { title: "Carbs" },
+      { title: "Fat" },
+      { title: "Protein" },
+    ],
+    "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+    "dom": '<"top pagination-center my-3" lf>t<"bottom pagination-center mt-3" ip><"clear">'
+});
 }
 
 $(main());
